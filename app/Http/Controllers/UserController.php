b36c5034d1c20556a3595c78d9d48b7988fa3e79
shamilka <sham.gurusinghe@gmail.com>
2025-06-01 20:27:58 +0530

    Refactored user sign in BE part.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Action\User\UserSignIn;
+use App\Http\Requests\User\UserSignInValidationRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -10,32 +12,12 @@ use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
-    public function userSignIn(Request $request)  : JsonResponse //return type declaration JsonResponse
+    public function userSignIn(UserSignInValidationRequest $request, UserSignIn $userSignIn): JsonResponse
     {
-//        Log::info($request->all());
-//        return ['test' =>'test'];
+        $validatedUserSignInRequest = $request->validated();
 
-        $user = User::where('email', $request->email)->first();
-//        Log::info($user);
+        return response()->json($userSignIn($validatedUserSignInRequest));
 
-        if(!$user){
-            return response()->json([
-                'message' => 'User not found'
-            ]);
-        }
-
-//        return response()->json([
-//            'message' => 'User found'
-//        ]);
-
-        if ($this->isValidateUserCredentials($request->all(),$user)){
-            return response()->json([
-//                'message' => 'User found',
-                'user_id' => $user->id,
-                'user_token'=> $user->createToken('testToken',['server:admin'])->plainTextToken,
-            ]);
-        }
-        return response()->json([]);
     }
 
     public function isValidateUserCredentials(array $request,User $user): bool// type definition - $request is a array and $user is User model / return boolean value
