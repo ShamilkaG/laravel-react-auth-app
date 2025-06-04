53181b5d9da1c515606b5bb2dd985011a9104da0
shamilka <sham.gurusinghe@gmail.com>
2025-06-04 17:03:02 +0530

    Create middleware for check user authentication with the sanctum token abilities.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\AdminUserMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,5 +12,10 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::post('/sign-in', [UserController::class, 'userSignIn']);
+//Route::get('/get-products-details', [ProductController::class, 'getProductsDetails']);
+Route::middleware(['auth:sanctum', AdminUserMiddleware::class])->group(function () {
+    Route::get('/get-products-details', [ProductController::class, 'getProductsDetails']);
+});
 
-Route::get('/get-products-details', [ProductController::class, 'getProductsDetails']);
+
+
